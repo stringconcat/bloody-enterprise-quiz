26f8676352d182ecf95a7643d3360ac1d93561ce
Sergey <sergey.bukharov@thoughtworks.com>
2021-07-11 00:09:18 +0800

    dedicated question x of y

--- src/QuestionBox.js
+++ src/QuestionBox.js
@@ -2,8 +2,14 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 export default function QuestionBox(props){
-    return (<Typography variant="h2" gutterBottom>
-        {props.text}
-        ({props.currentIndex + 1}/{props.totalQuestions})
-    </Typography>)
+    return (
+    <>
+        <Typography variant="subtitle1" gutterBottom>
+            Вопрос {props.currentIndex + 1} из {props.totalQuestions}
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+            {props.text}
+        </Typography>
+    </>
+    )
 }
