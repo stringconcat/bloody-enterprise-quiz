f10ddd78c21afe6fa697efe832d15e7965bdbd80
Sergey <sergey.bukharov@thoughtworks.com>
2021-07-11 00:26:36 +0800

    add pages

---
+++ src/IntroPage.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+export default function IntroPage() {
+
+	return (
+		<Grid
+			container
+			direction="row"
+			justifyContent="center">
+
+			<Grid
+				container
+				spacing="3"
+				direction="column"
+				justifyContent="flex-start"
+				alignItems="stretch"
+				item xs={6}>
+
+				<Grid item>
+                <>
+                    <Typography variant="subtitle1" gutterBottom>
+                        StringConcat тест
+                    </Typography>
+                    <Typography variant="h3" gutterBottom>
+                        17 вопросов для оценки кровавости энтерпрайза
+                    </Typography>
+                 </>
+				</Grid>	
+                <Grid item>
+                <>
+                    <Typography variant="body" gutterBottom>
+                        Мы составили тест, который поможет вам дистанционно оценить глубину кроличьей норы. Пройдите сами, чтобы проверить текущую команду, или отправьте потенциальному работодателю.
+                        Тест вдохновлён опросником Джоэля Спольски, который придумал StackOverflow. Опроснику уже двадцать лет, так что мы его немного расширили и доработали.
+                    </Typography>
+                 </>
+				</Grid>	
+
+                <Grid item>
+					<Button variant="outlined" color="primary">Начать</Button>
+				</Grid>
+
+			</Grid>
+		</Grid>
+	);
+}
---
+++ src/QuizPage.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import { questions } from './Questions';
+import DescriptionBox from './DescriptionBox';
+import QuestionBox from './QuestionBox';
+import AnswersGroup from './AnswersGroup';
+
+export default function QuizzPage() {
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0)
+	const [currentAnswerIndex, setCurrentAnswerIndex] = useState(-1)
+
+	const handleNextQuestionButton = (scoreFortheAnswer) => {
+		const currentAnswerScore = questions[currentQuestion].answerOptions[currentAnswerIndex].score
+		setScore(score + currentAnswerScore)
+
+		const nextQeustion = currentQuestion + 1;
+		if (nextQeustion < questions.length) {
+			setCurrentQuestion(nextQeustion)
+		} else {
+			setShowScore(true)
+		}
+		setCurrentAnswerIndex(-1)
+	}
+
+
+	  const handleAnswerChange = (event) => {
+		setCurrentAnswerIndex(event.target.value)
+	  };
+
+	return (
+		<Grid
+			container
+			direction="row"
+			justifyContent="center">
+
+			<Grid
+				container
+				spacing="3"
+				direction="column"
+				justifyContent="flex-start"
+				alignItems="stretch"
+				item xs={6}>
+
+				<Grid item>
+					<QuestionBox 
+						text={questions[currentQuestion].questionText}
+						currentIndex={currentQuestion}
+						totalQuestions={questions.length}
+					/>
+				</Grid>	
+
+				<Grid item>
+					{showScore ? (
+						<div className='score-section'>You scored {score} out of {questions.length}</div>
+					) : (
+						<AnswersGroup 
+							onChange={handleAnswerChange}
+							answers={questions[currentQuestion].answerOptions}
+							currentAnswerIndex={currentAnswerIndex}
+						/>
+					)}
+				</Grid>	
+
+				<Grid item>
+					{currentAnswerIndex !== -1 ? (
+					<DescriptionBox description={questions[currentQuestion].description} />
+				) : (<></>) 
+				}
+				</Grid>
+
+				<Grid item>
+					{currentAnswerIndex !== -1 ? (
+						<Button variant="outlined" color="primary" onClick={() => handleNextQuestionButton()}>Следующий вопрос</Button>
+						) : (<></>) 
+					}
+				</Grid>
+			</Grid>
+		</Grid>
+	);
+}
