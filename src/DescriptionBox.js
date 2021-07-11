5b75fd8238e42960c39e7ea206fed122e243bb16
Sergey <sergey.bukharov@thoughtworks.com>
2021-07-11 12:53:24 +0800

    add html tags for description

--- src/DescriptionBox.js
+++ src/DescriptionBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import ReactHtmlParser from "react-html-parser";
 
 export default function DescriptionBox(props) {
 
@@ -9,7 +10,7 @@ export default function DescriptionBox(props) {
         <Card variant="outlined">
         <CardContent>
             <Typography  variant="body1" gutterBottom>
-                {props.description}
+                {ReactHtmlParser(props.description)}
             </Typography>
         </CardContent>
         </Card>	
