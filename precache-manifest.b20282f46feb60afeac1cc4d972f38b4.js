647146162f2dabcd271b7afb84d109aba734a74a
Sergey <sergey.bukharov@thoughtworks.com>
2021-07-11 10:55:18 +0800

    Updates

---
+++ precache-manifest.b20282f46feb60afeac1cc4d972f38b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164a44a3dc766b9747cc8533a73e6a34",
+    "url": "/.github.io/%7Bbloody-enterprise-quiz/index.html"
+  },
+  {
+    "revision": "ef82cb247a2107f09afb",
+    "url": "/.github.io/%7Bbloody-enterprise-quiz/static/js/2.77461ae6.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/.github.io/%7Bbloody-enterprise-quiz/static/js/2.77461ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cc16ad0de7c4beefd9",
+    "url": "/.github.io/%7Bbloody-enterprise-quiz/static/js/main.9d2beb83.chunk.js"
+  },
+  {
+    "revision": "87e5cfacd732251af605",
+    "url": "/.github.io/%7Bbloody-enterprise-quiz/static/js/runtime-main.0f9e17d1.js"
+  }
+]);
