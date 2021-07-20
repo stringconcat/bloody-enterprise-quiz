18e85dabc199882b0ad6bc9bd0ffe8008bff3579
Sergey <sergey.bukharov@thoughtworks.com>
2021-07-20 13:47:55 +0800

    Updates

---
+++ precache-manifest.8998731c62ece5d09e6362d792a0ffd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b914b24b82e4a6ebbd0dece43e56ca99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec7413cd58254ea6289b",
+    "url": "./static/js/2.bd0201bb.chunk.js"
+  },
+  {
+    "revision": "3f0e7270d9361d86cdedbbc2d6ca185f",
+    "url": "./static/js/2.bd0201bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67eaf2606109338cee3a",
+    "url": "./static/js/main.db50f546.chunk.js"
+  },
+  {
+    "revision": "d2c4caa5c6ba89789e56",
+    "url": "./static/js/runtime-main.7a079ac7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc144525ae47d4d71cc02b323d2fb05a.js"
+  "./precache-manifest.8998731c62ece5d09e6362d792a0ffd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
