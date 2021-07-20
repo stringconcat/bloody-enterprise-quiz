e57942fcb797c8428b765fda6d34f09723d0700d
Sergey <sergey.bukharov@thoughtworks.com>
2021-07-20 20:45:32 +0800

    Updates

---
+++ precache-manifest.c6d50fed7553b7eed4afb6bce7386223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0de4d324591674778551f6877f6977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4905691d577d259f4203",
+    "url": "/static/js/2.c5ceb839.chunk.js"
+  },
+  {
+    "revision": "3f0e7270d9361d86cdedbbc2d6ca185f",
+    "url": "/static/js/2.c5ceb839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e47010654505f1635a5",
+    "url": "/static/js/main.fc4035c7.chunk.js"
+  },
+  {
+    "revision": "1c63a847d82dfbff8cc3",
+    "url": "/static/js/runtime-main.7a97b2be.js"
+  }
+]);
